43286293bb0eede4b89aada578bb5bd482018a5d
Jidaewook <psatdoctor@naver.com>
2020-07-05 19:14:50 +0900

    Setting - Stack, Tab Navigation

---
+++ navigation/StackNavigator.js
@@ -0,0 +1,86 @@
+import React from 'react'
+import {createStackNavigator} from "@react-navigation/stack";
+
+import Home from "../screen/Home";
+import Contact from "../screen/Contact";
+import About from "../screen/About";
+import Icon from "react-native-vector-icons/Ionicons";
+import { StackActions } from '@react-navigation/native';
+
+const stack = createStackNavigator();
+
+const screenOptionStyles = {
+    headerStyle: {
+        backgroundColor: "#819AD4"
+    },
+    headerTintColor: "white",
+    headerBackTitle: "Back"
+};
+
+const MainStackNavigator = ({navigation}) => {
+    return (
+        <stack.Navigator 
+            screenOptions={screenOptionStyles}
+        >
+            <stack.Screen 
+                name="Home"
+                component={Home}
+                options={{
+                    title: 'Overview',
+                    headerLeft: () => (
+                        <Icon.Button 
+                            name="ios-menu" 
+                            size={25} 
+                            backgroundColor="#819AD4" 
+                            onPress={() => navigation.openDrawer()}
+                        />
+                    )
+                }}
+            />
+            <stack.Screen 
+                name="About"
+                component={About}
+                options={{
+                    title: 'About',
+                    headerLeft: () => (
+                        <Icon.Button 
+                            name="ios-menu" 
+                            size={25} 
+                            backgroundColor="#819AD4" 
+                            onPress={() => navigation.openDrawer()}
+                        />
+                    )
+                }}
+            />
+        </stack.Navigator>
+    )
+};
+
+const ContactStackNavigator = ({navigation}) => {
+    return (
+        <stack.Navigator
+            screenOptions={screenOptionStyles}
+        >
+            <stack.Screen 
+                name="Contact"
+                component={Contact}
+                options={{
+                    title: 'Contact',
+                    headerLeft: () => (
+                        <Icon.Button 
+                            name="ios-menu" 
+                            size={25} 
+                            backgroundColor="#96c0eb" 
+                            onPress={() => navigation.openDrawer()}
+                        />
+                    )
+                }}
+            />
+
+        </stack.Navigator>
+
+    )
+
+}
+
+export {MainStackNavigator, ContactStackNavigator}
---
+++ navigation/TabNavigator.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+
+import {MainStackNavigator, ContactStackNavigator} from "./StackNavigator";
+
+const Tabs = createBottomTabNavigator();
+
+const BottomTabnavigator = () => {
+    return (
+        <Tabs.Navigator>
+            <Tabs.Screen name="Home" component={MainStackNavigator} />
+            <Tabs.Screen name="Contact" component={ContactStackNavigator} />
+        </Tabs.Navigator>
+    )
+};  
+
+expot default BottomTabnavigator;
